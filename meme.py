8916c928b46cdc3390d0169e87f16ad2b373f686
FridLexman <jacdowell0@gmail.com>
2022-11-21 22:38:54 -0500

    feature/jake meme.py update

--- meme.py
+++ meme.py
@@ -1,15 +1,18 @@
 import discord
 from discord.ext import commands
 
+MB_DICT = ['MrBeast', 'Mr Beast', 'mr beast', 'MRBEAST']
 class meme(commands.Cog):
     def __init__(self, client):
         self.client = client
         
         @client.event
         async def on_message(message):
-            if message.content.startswith('Mr Beast'):
+            if any(word in message.content.lower() for word in MB_DICT):
                 await message.channel.send('I LOVE MR. BEAST!!!')# if the message is 'Mr. Beast', the bot responds with 'I LOVE MR. BEAST!!!'
                 return
-            
+            else:
+                await client.process_commands(message)
+                
 def setup(client):
     client.add_cog(meme(client))
