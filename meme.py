1e6390134738d68108542ab696e29ef14c8d2176
Alex Daughenbaugh <dbaugh1999@gmail.com>
2022-11-22 15:44:30 -0500

    adding disconnect messaging

--- meme.py
+++ meme.py
@@ -1,17 +1,29 @@
+from discord import *
 import discord
 from discord.ext import commands
+import random
+import os
 
 class meme(commands.Cog):
-    def __init__(self, client):
+    def __init__(self, client:Client):
         self.client = client
 
         @client.event
         async def on_message(message):
             if message.author == client.user:
                 return
-            if "based" in message.content:
+            if "based" in message.content.lower():
                 await message.reply("based on WHAT?")
+
             await client.process_commands(message)
+
+        @client.event
+        async def on_voice_state_update(member:Member, before, after:VoiceState):
+            if after.channel == None and before.channel.guild.name == "Jakoby's Safe Haven":
+                mediaFiles = os.listdir("media/.")
+                fileIndex = random.randint(0, len(mediaFiles)-1)
+                channel:TextChannel = client.get_channel(949810707535376415)
+                await channel.send(f"Get some GOOD SLEEP! {member.mention}", file=discord.File(f"media/{mediaFiles[fileIndex]}"), delete_after=20)
 
 def setup(client):
     client.add_cog(meme(client))
